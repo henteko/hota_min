37352d3376939848c736213622d961cd1ed4efcf
henteko <henteko07@gmail.com>
2013-06-15 16:04:02 +0900

    add webfront

---
+++ webfront/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webfront::Application.config.secret_token = 'a685092fa5789706fbcee7a546b9e37df211e206c5fc0e55f1e906ce874b879bdb8cda58be1f75a49badaf15b6985d75155e6c813777fcb5578a9bc17e9ab43c'
